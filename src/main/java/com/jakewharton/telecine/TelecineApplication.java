acd63001f903a9fd5066af134cadcede401cd2ea
Jake Wharton <jw@squareup.com>
2014-10-23 14:41:40 -0700

    Crashlytics via Fabric...

--- src/main/java/com/jakewharton/telecine/TelecineApplication.java
+++ src/main/java/com/jakewharton/telecine/TelecineApplication.java
@@ -1,6 +1,8 @@
 package com.jakewharton.telecine;
 
 import android.app.Application;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 import timber.log.Timber;
 
 public final class TelecineApplication extends Application {
@@ -9,6 +11,9 @@ public final class TelecineApplication extends Application {
 
     if (BuildConfig.DEBUG) {
       Timber.plant(new Timber.DebugTree());
+    } else {
+      Fabric.with(this, new Crashlytics());
+      Timber.plant(new CrashlyticsTree());
     }
   }
 }
