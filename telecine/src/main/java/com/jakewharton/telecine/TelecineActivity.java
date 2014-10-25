6679643b25b3b28f3d93295d53508395df6d2278
Jake Wharton <jw@squareup.com>
2014-10-24 21:42:30 -0700

    WIP

--- telecine/src/main/java/com/jakewharton/telecine/TelecineActivity.java
+++ telecine/src/main/java/com/jakewharton/telecine/TelecineActivity.java
@@ -1,5 +1,10 @@
 package com.jakewharton.telecine;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
 import android.app.Activity;
 import android.app.ActivityManager;
 import android.content.Intent;
@@ -7,6 +12,10 @@ import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.ViewTreeObserver;
 import android.widget.Spinner;
 import android.widget.Switch;
 import butterknife.ButterKnife;
@@ -19,7 +28,15 @@ import com.google.android.gms.analytics.HitBuilders;
 import javax.inject.Inject;
 import timber.log.Timber;
 
+import static android.view.View.ALPHA;
+import static android.view.View.TRANSLATION_Y;
+
 public final class TelecineActivity extends Activity {
+  private static final int DURATION_TUTORIAL_TRANSITION = 300;
+
+  @InjectView(R.id.main) View mainView;
+  @InjectView(R.id.tutorial) View tutorialView;
+
   @InjectView(R.id.spinner_video_size_percentage) Spinner videoSizePercentageView;
   @InjectView(R.id.switch_show_countdown) Switch showCountdownView;
   @InjectView(R.id.switch_hide_from_recents) Switch hideFromRecentsView;
@@ -32,9 +49,15 @@ public final class TelecineActivity extends Activity {
 
   private VideoSizePercentageAdapter videoSizePercentageAdapter;
   private int longClickCount;
+  private boolean showingTutorial;
+  private float defaultActionBarElevation;
+  private int primaryColor;
+  private int primaryColorDark;
+  private int mainAnimationTranslationY;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    ((TelecineApplication) getApplication()).inject(this);
 
     Resources res = getResources();
     String taskName = res.getString(R.string.app_name);
@@ -43,10 +66,28 @@ public final class TelecineActivity extends Activity {
     int taskColor = res.getColor(R.color.primary_normal);
     setTaskDescription(new ActivityManager.TaskDescription(taskName, taskIcon, taskColor));
 
-    ((TelecineApplication) getApplication()).inject(this);
+    defaultActionBarElevation = getActionBar().getElevation();
+
+    TypedValue value = new TypedValue();
+    Resources.Theme theme = getTheme();
+    theme.resolveAttribute(android.R.attr.colorPrimary, value, true);
+    primaryColor = value.data;
+    theme.resolveAttribute(android.R.attr.colorPrimaryDark, value, true);
+    primaryColorDark = value.data;
+
+    mainAnimationTranslationY =
+        getResources().getDimensionPixelSize(R.dimen.main_animation_translation_y);
 
     setContentView(R.layout.activity_main);
     ButterKnife.inject(this);
+
+    tutorialView.getViewTreeObserver()
+        .addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+          @Override public void onGlobalLayout() {
+            tutorialView.setTranslationY(tutorialView.getHeight());
+            tutorialView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+          }
+        });
 
     videoSizePercentageAdapter = new VideoSizePercentageAdapter(this);
 
@@ -120,6 +161,79 @@ public final class TelecineActivity extends Activity {
           .setValue(newValue ? 1 : 0)
           .build());
     }
+  }
+
+  @Override public void onBackPressed() {
+    if (showingTutorial) {
+      hideTutorial();
+    } else {
+      super.onBackPressed();
+    }
+  }
+
+  @OnClick(R.id.view_tutorial) void showTutorial() {
+    showingTutorial = true;
+
+    // Animate status bar color to primary.
+    Animator statusBar = ObjectAnimator.ofArgb(getWindow(), "statusBarColor", primaryColor);
+
+    // Animate tutorial up.
+    tutorialView.setVisibility(View.VISIBLE);
+    Animator tutorial = ObjectAnimator.ofFloat(tutorialView, TRANSLATION_Y, 0);
+
+    // Animate main down and alpha out.
+    PropertyValuesHolder main1 =
+        PropertyValuesHolder.ofFloat(TRANSLATION_Y, mainAnimationTranslationY);
+    PropertyValuesHolder main2 = PropertyValuesHolder.ofFloat(ALPHA, 0);
+    Animator main = ObjectAnimator.ofPropertyValuesHolder(mainView, main1, main2);
+
+    // Animate elevation of action bar to 0.
+    Animator actionBar = ObjectAnimator.ofFloat(getActionBar(), "elevation", 0);
+
+    AnimatorSet set = new AnimatorSet();
+    set.playTogether(statusBar, tutorial, main, actionBar);
+    set.setDuration(DURATION_TUTORIAL_TRANSITION);
+    set.addListener(new AnimatorListenerAdapter() {
+      @Override public void onAnimationEnd(Animator animation) {
+        getActionBar().setTitle(R.string.tutorial_welcome);
+      }
+    });
+    set.start();
+  }
+
+  private void hideTutorial() {
+    showingTutorial = false;
+
+    // Animate status bar color to primary dark.
+    Animator statusBar = ObjectAnimator.ofArgb(getWindow(), "statusBarColor", primaryColorDark);
+
+    // Animate tutorial down.
+    int tutorialHeight = tutorialView.getHeight();
+    Animator tutorial = ObjectAnimator.ofFloat(tutorialView, TRANSLATION_Y, tutorialHeight);
+    tutorial.addListener(new AnimatorListenerAdapter() {
+      @Override public void onAnimationEnd(@NonNull Animator animation) {
+        tutorialView.setVisibility(View.INVISIBLE);
+      }
+    });
+
+    // Animate main up and alpha in.
+    PropertyValuesHolder main1 = PropertyValuesHolder.ofFloat(TRANSLATION_Y, 0);
+    PropertyValuesHolder main2 = PropertyValuesHolder.ofFloat(ALPHA, 1);
+    Animator main = ObjectAnimator.ofPropertyValuesHolder(mainView, main1, main2);
+
+    // Animate elevation of action bar back to normal.
+    Animator actionBar =
+        ObjectAnimator.ofFloat(getActionBar(), "elevation", defaultActionBarElevation);
+
+    AnimatorSet set = new AnimatorSet();
+    set.playTogether(statusBar, tutorial, main, actionBar);
+    set.setDuration(DURATION_TUTORIAL_TRANSITION);
+    set.addListener(new AnimatorListenerAdapter() {
+      @Override public void onAnimationStart(Animator animation) {
+        getActionBar().setTitle(R.string.app_name);
+      }
+    });
+    set.start();
   }
 
   @Override protected void onActivityResult(int requestCode, int resultCode, Intent data) {
