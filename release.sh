b1ac8e410290662a95c3e50730a980f785c61416
Jake Wharton <jw@squareup.com>
2014-10-24 23:55:11 -0700

    Prepare for first beta build.

---
+++ release.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+if [[ $# -ne 1 ]]; then
+  echo "Supply keystore as the sole argument to the script."
+  exit 1
+fi
+
+REPO_DIR="$( cd "$( dirname "$0" )" && pwd )"
+
+UNSIGNED="$REPO_DIR/build/outputs/apk/telecine-release-unsigned.apk"
+UNALIGNED="$REPO_DIR/build/outputs/apk/telecine-release-unaligned.apk"
+FINAL="$REPO_DIR/build/outputs/apk/telecine-release.apk"
+
+if [[ $(git status 2> /dev/null | tail -n1) != "nothing to commit, working directory clean" ]]; then
+  echo "Working directory dirty. Please revert or commit."
+  exit 1
+fi
+
+set -ex
+
+$REPO_DIR/gradlew -p "$REPO_DIR" clean assembleRelease -Dpre-dex=false
+
+jarsigner -sigalg SHA1withRSA -digestalg SHA1 -sigfile CERT -keystore "$1" -signedjar "$UNALIGNED" "$UNSIGNED" jakewharton
+
+$ANDROID_HOME/build-tools/21.0.0/zipalign 4 "$UNALIGNED" "$FINAL"
