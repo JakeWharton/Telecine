4a7129fd93744961738ce99fb4f990c16d176f87
Jake Wharton <jw@squareup.com>
2016-06-19 02:45:01 -0400

    Get rid of needless  modifiers.

--- telecine/src/main/java/com/jakewharton/telecine/TelecineComponent.java
+++ telecine/src/main/java/com/jakewharton/telecine/TelecineComponent.java
@@ -6,8 +6,8 @@ import javax.inject.Singleton;
 @Singleton
 @Component(modules = TelecineModule.class)
 interface TelecineComponent {
-  void inject(final TelecineActivity activity);
-  void inject(final TelecineService service);
-  void inject(final TelecineShortcutConfigureActivity activity);
-  void inject(final TelecineShortcutLaunchActivity activity);
+  void inject(TelecineActivity activity);
+  void inject(TelecineService service);
+  void inject(TelecineShortcutConfigureActivity activity);
+  void inject(TelecineShortcutLaunchActivity activity);
 }
