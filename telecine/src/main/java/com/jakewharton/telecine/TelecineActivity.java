dalangge1/Telecine-and

update to dagger 2

telecine/src/main/java/com/jakewharton/telecine/TelecineActivity.java
@@ -52,7 +52,7 @@ public final class TelecineActivity extends AppCompatActivity {
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
 
-    ((TelecineApplication) getApplication()).inject(this);
+    ((TelecineApplication) getApplication()).getTelecineComponent().inject(this);
 
     setContentView(R.layout.activity_main);
     ButterKnife.bind(this);

telecine/src/main/java/com/jakewharton/telecine/TelecineService.java
@@ -8,9 +8,10 @@ import android.content.Intent;
 import android.os.IBinder;
 import android.provider.Settings;
 import android.support.annotation.NonNull;
+import timber.log.Timber;
+
 import javax.inject.Inject;
 import javax.inject.Provider;
-import timber.log.Timber;
 
 import static android.app.Notification.PRIORITY_MIN;
 
@@ -90,7 +91,7 @@ public final class TelecineService extends Service {
       throw new IllegalStateException("Result code or data missing.");
     }
 
-    ((TelecineApplication) getApplication()).inject(this);
+    ((TelecineApplication) getApplication()).getTelecineComponent().inject(this);
 
     recordingSession =
         new RecordingSession(this, listener, resultCode, data, analytics, showCountdownProvider,

telecine/src/main/java/com/jakewharton/telecine/TelecineShortcutConfigureActivity.java
@@ -14,7 +14,7 @@ public final class TelecineShortcutConfigureActivity extends Activity {
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
 
-    ((TelecineApplication) getApplication()).inject(this);
+    ((TelecineApplication) getApplication()).getTelecineComponent().inject(this);
     analytics.send(new HitBuilders.EventBuilder() //
         .setCategory(Analytics.CATEGORY_SHORTCUT) //
         .setAction(Analytics.ACTION_SHORTCUT_ADDED) //

telecine/src/main/java/com/jakewharton/telecine/TelecineShortcutLaunchActivity.java
@@ -11,7 +11,7 @@ public final class TelecineShortcutLaunchActivity extends Activity {
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    ((TelecineApplication) getApplication()).inject(this);
+    ((TelecineApplication) getApplication()).getTelecineComponent().inject(this);
 
     analytics.send(new HitBuilders.EventBuilder() //
         .setCategory(Analytics.CATEGORY_SHORTCUT)

JakeWharton/Telecine

telecine/src/main/java/com/jakewharton/telecine/TelecineActivity.java
@@ -52,7 +52,7 @@ public final class TelecineActivity extends AppCompatActivity {
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
 
-    ((TelecineApplication) getApplication()).inject(this);
+    ((TelecineApplication) getApplication()).injector().inject(this);
 
     setContentView(R.layout.activity_main);
     ButterKnife.bind(this);

telecine/src/main/java/com/jakewharton/telecine/TelecineService.java
@@ -8,9 +8,10 @@ import android.content.Intent;
 import android.os.IBinder;
 import android.provider.Settings;
 import android.support.annotation.NonNull;
+import timber.log.Timber;
+
 import javax.inject.Inject;
 import javax.inject.Provider;
-import timber.log.Timber;
 
 import static android.app.Notification.PRIORITY_MIN;
 
@@ -90,7 +91,7 @@ public final class TelecineService extends Service {
       throw new IllegalStateException("Result code or data missing.");
     }
 
-    ((TelecineApplication) getApplication()).inject(this);
+    ((TelecineApplication) getApplication()).injector().inject(this);
 
     recordingSession =
         new RecordingSession(this, listener, resultCode, data, analytics, showCountdownProvider,

telecine/src/main/java/com/jakewharton/telecine/TelecineShortcutConfigureActivity.java
@@ -14,7 +14,7 @@ public final class TelecineShortcutConfigureActivity extends Activity {
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
 
-    ((TelecineApplication) getApplication()).inject(this);
+    ((TelecineApplication) getApplication()).injector().inject(this);
     analytics.send(new HitBuilders.EventBuilder() //
         .setCategory(Analytics.CATEGORY_SHORTCUT) //
         .setAction(Analytics.ACTION_SHORTCUT_ADDED) //

telecine/src/main/java/com/jakewharton/telecine/TelecineShortcutLaunchActivity.java
@@ -11,7 +11,7 @@ public final class TelecineShortcutLaunchActivity extends Activity {
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    ((TelecineApplication) getApplication()).inject(this);
+    ((TelecineApplication) getApplication()).injector().inject(this);
 
     analytics.send(new HitBuilders.EventBuilder() //
         .setCategory(Analytics.CATEGORY_SHORTCUT)